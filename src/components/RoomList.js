1fb27895f91b26a57492386d379881bbcf88f4d4
fbadruddin <farooqbadruddin@gmail.com>
2019-02-16 16:10:30 -0800

    completed the ability to add a new chat room

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -16,14 +16,30 @@ class RoomList extends Component {
       });
   }
 
+  createRoom(e) {
+      e.preventDefault();
+      let newRoom = document.getElementById('newRoom');
+      if(newRoom.value.length===0) return;
+
+      this.roomsRef.push(newRoom.value);
+      newRoom.value='';
+  }
+
   render() {
       return (
         <div className="rooms">
             <ul className="rooms-center">
-                {this.state.rooms.map(room => 
-                    <li key={room.key}>{room.value}</li>    
-                )}
-            </ul>    
+            {
+                this.state.rooms.map(room => 
+                    <li key={room.key}>{room.value}</li>
+                )
+            }
+            </ul>
+            <form>
+                <input className="form-control" type="text" id="newRoom" />
+                <p></p>
+                <input type="button" id="create" className="btn btn-primary" value="Create" onClick={(e) => this.createRoom(e)} />
+            </form>
         </div> 
       )
   }
